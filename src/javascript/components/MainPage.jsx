e7a8fedbf441253cc65973e887043302aab93ad3
iulia sirbu <iuliasirbu47@gmail.com>
2022-05-06 18:35:44 +0300

    Initial setup

---
+++ src/javascript/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage(props){
+    return(
+        <div id="MainPage">
+            <h1 className='mt-10 text-blue-600 text-3x1 font-bold'>Main Page</h1>
+        </div>
+    );
+}
+
+export default MainPage;
